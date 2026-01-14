f7eaf6a7b051672fd27acf3b5491fe72646a4b0a
Purushottam Khedre <purushottam.khedre@hotwax.co>
2026-01-14 16:50:52 +0530

    Added the missing computation and changes as per reviews

--- script/co/hotwax/shopify/return/MapShopifyRefundToReturn.groovy
+++ script/co/hotwax/shopify/return/MapShopifyRefundToReturn.groovy
@@ -1,6 +1,7 @@
 
 import java.math.RoundingMode
 import co.hotwax.shopify.util.ShopifyHelper
+import org.moqui.util.CollectionUtilities
 
 shopifyShop = ec.entity.find("co.hotwax.shopify.ShopifyShop").condition("shopId", shopId).useCache(true).one()
 productStore = shopifyShop?.'org.apache.ofbiz.product.store.ProductStore'
@@ -31,10 +32,87 @@ def returnReasonMap = refund.return?.returnLineItems?.collectEntries {
 refundId = ShopifyHelper.resolveShopifyGid(refund.id)
 shopifyOrderId = ShopifyHelper.resolveShopifyGid(order.id) // TODO: Remove as already they will be in the context
 
+
+BigDecimal totalReturnAmount = 0.0
+
+def itemsList = refund.refundLineItems.collect { rli ->
+    def lineItem = rli.lineItem
+    def qtyRatio = rli.quantity / lineItem.quantity
+    def isRestocked = rli.restockType && rli.restockType != 'no_restock'
+    def prorate = { val -> (val as BigDecimal * qtyRatio).setScale(2, RoundingMode.HALF_UP) }
+
+    def productId = ec.entity.find("co.hotwax.shopify.ShopifyShopProduct").condition("shopId", shopId).condition("shopifyProductId", ShopifyHelper.resolveShopifyGid(lineItem.variant?.id)).selectField("productId").useCache(true).one()?.productId
+
+    if(!productId) {
+        prodIdentifications = ec.entity.find("org.apache.ofbiz.product.product.GoodIdentification").condition(["productId": productId, "goodIdentificationTypeId": "SKU"]).selectField("productId,fromDate,thruDate").useCache(true).list()
+        productId = CollectionUtilities.filterMapListByDate(prodIdentifications, null, null, ec.user.nowTimestamp).get(0)?.productId
+    }
+    
+    def isCustomGiftCard = lineItem.isGiftCard && !(lineItem.variant?.id)
+
+    // Calculate item total for header
+    def itemAmt = (rli.subtotalSet?.shopMoney?.amount as BigDecimal) ?: 0.0
+    def itemTax = (rli.totalTaxSet?.shopMoney?.amount as BigDecimal) ?: 0.0
+    totalReturnAmount += (itemAmt + itemTax)
+
+    [
+        id: productId, // TODO: Handle custom gift card productId 
+        itemExternalId: ShopifyHelper.resolveShopifyGid(rli.id), // Refund Line Item ID
+        orderItemExternalId: ShopifyHelper.resolveShopifyGid(lineItem.id), // Order Line Item ID
+        quantity: rli.quantity,
+        status: "RETURN_COMPLETED",
+        price: rli.priceSet.shopMoney.amount,
+        returnType: "RTN_REFUND",
+        restockType: isRestocked ? 'INV_RETURNED' : 'INV_NOT_RETURNED',
+        itemTypeId: isRestocked ? 'RET_FPROD_ITEM' : 'RET_LOST_ITEM',
+        receivedQty: isRestocked ? rli.quantity : 0,
+        reason: returnReasonMap?.get(ShopifyHelper.resolveShopifyGid(lineItem.id)) ?: 'UNKNOWN',
+        includeAdjustments: 'N',
+        itemAdjustments: (
+            lineItem.discountAllocations.collect { [
+                type: "RET_EXT_PRM_ADJ", 
+                description: "External Discount", 
+                amount: prorate(it.allocatedAmountSet.shopMoney.amount), 
+                comments: "Prorated discount",
+            ] } +
+            lineItem.taxLines.collect { [
+                type: "RET_SALES_TAX_ADJ", 
+                description: it.title, 
+                amount: prorate(it.priceSet.shopMoney.amount), 
+                sourcePercentage: it.rate * 100, 
+                comments: "${it.title} (${it.rate * 100}%)"
+            ] }
+        )
+    ].with { m -> if(!isOrphan) m.orderExternalId = shopifyOrderId; m }
+}
+
+
+def adjustmentsList = (
+    // Shipping 
+    refund.refundShippingLines.collectMany { rsl ->
+        def shipAmt = (rsl.subtotalAmountSet?.shopMoney?.amount as BigDecimal) ?: 0.0
+        def taxAmt = (rsl.taxAmountSet?.shopMoney?.amount as BigDecimal) ?: 0.0
+        
+        totalReturnAmount += (shipAmt + taxAmt)
+
+        [
+            [type: "RET_SHIPPING_ADJ", amount: shipAmt, description: "Shipping Refund"],
+            [type: "RET_SALES_TAX_ADJ", amount: taxAmt, description: "Shipping Tax Refund"]
+        ]
+    } +
+    // Order Adjustments
+    refund.orderAdjustments.collect { oa ->
+        def amount = (oa.amountSet?.shopMoney?.amount as BigDecimal ?: 0.0).multiply(-1)
+        
+        totalReturnAmount += amount
+
+        [type: "APPEASEMENT", amount: amount, description: "Refund Adjustment for orderId ${shopifyOrderId}", comments: "Shopify Order Adjustment"]
+    }
+).findAll { it.amount }
+
 result = [
     payLoad: [
         externalId: refundId,
-        orderExternalId: shopifyOrderId,
         type: shipToFacilityId == "_NA_" ? "APPEASEMENT" : (refund.attributionType ?: "CUSTOMER_RETURN"),
         status: "RETURN_COMPLETED",
         companyId: productStore.payToPartyId ?: '_NA_',
@@ -45,65 +123,19 @@ result = [
         shipTo: [facilityId: shipToFacilityId],
         currencyCode: refund.totalRefundedSet.shopMoney.currencyCode,
         grandTotal: refund.totalRefundedSet.shopMoney.amount,
-        items: refund.refundLineItems.collect { rli ->
-            def lineItem = rli.lineItem
-            def qtyRatio = rli.quantity / lineItem.quantity
-            def isRestocked = rli.restockType && rli.restockType != 'no_restock'
-            def prorate = { val -> (val as BigDecimal * qtyRatio).setScale(2, RoundingMode.HALF_UP) }
-
-            [
-                id: ShopifyHelper.resolveShopifyGid(lineItem.variant?.id), // TODO: Requires internal productId resolution
-                itemExternalId: ShopifyHelper.resolveShopifyGid(rli.id), // Refund Line Item ID
-                orderItemExternalId: ShopifyHelper.resolveShopifyGid(lineItem.id), // Order Line Item ID
-                sku: lineItem.sku,
-                quantity: rli.quantity,
-                status: "RETURN_COMPLETED",
-                price: rli.priceSet.shopMoney.amount,
-                returnType: "RTN_REFUND",
-                restockType: isRestocked ? 'INV_RETURNED' : 'INV_NOT_RETURNED',
-                itemTypeId: isRestocked ? 'RET_FPROD_ITEM' : 'RET_LOST_ITEM',
-                receivedQty: isRestocked ? rli.quantity : 0,
-                reason: returnReasonMap?.get(ShopifyHelper.resolveShopifyGid(lineItem.id)) ?: 'UNKNOWN',
-                itemAdjustments: (
-                    lineItem.discountAllocations.collect { [
-                        type: "RET_EXT_PRM_ADJ", 
-                        description: "External Discount", 
-                        amount: prorate(it.allocatedAmountSet.shopMoney.amount), 
-                        comments: "Prorated discount"
-                    ] } +
-                    lineItem.taxLines.collect { [
-                        type: "RET_SALES_TAX_ADJ", 
-                        description: it.title, 
-                        amount: prorate(it.priceSet.shopMoney.amount), 
-                        sourcePercentage: it.rate * 100, 
-                        comments: "${it.title} (${it.rate * 100}%)"
-                    ] }
-                )
-            ]
-        },
-        returnAdjustment: (
-            refund.refundShippingLines.collectMany {[
-                [type: "RET_SHIPPING_ADJ", amount: it.subtotalAmountSet?.shopMoney?.amount, description: "Shipping Refund"],
-                [type: "RET_SALES_TAX_ADJ", amount: it.taxAmountSet?.shopMoney?.amount, description: "Shipping Tax Refund"]
-            ]} +
-            refund.orderAdjustments.collect { 
-                [type: "APPEASEMENT", amount: it.amountSet?.shopMoney?.amount, description: "Refund Adjustment for orderId ${shopifyOrderId}", comments: "Shopify Order Adjustment"] 
-            }
-        ).findAll { it.amount },
+        totalReturnAmount: totalReturnAmount,
+        items: itemsList,
+        returnAdjustment: adjustmentsList,
         returnPaymentPref: refund.transactions
             .findAll { it.kind == "REFUND" && it.status == "SUCCESS" }
-            .collect {[
-                paymentMethodTypeId: "EXT_SHOP_OTHR_GTWAY",
-                statusId: "PAYMENT_REFUNDED",
-                manualRefNum: ShopifyHelper.resolveShopifyGid(it.id),
-                parentRefNum: ShopifyHelper.resolveShopifyGid(it.parentTransaction?.id),
-                presentmentCurrency: it.amountSet.presentmentMoney.currencyCode,
-                maxAmount: it.amountSet.shopMoney.amount,
-                presentmentAmount: it.amountSet.presentmentMoney.amount
-            ]},
-        returnIdentifications: [
-            [returnIdentificationTypeId: "SHOPIFY_RTN_ID", idValue: refundId],
-            [returnIdentificationTypeId: "SHPY_ORPN_RTN_ORD_ID", idValue: ShopifyHelper.resolveShopifyGid(order.id)]
-        ]
+            .collect {
+                txMappingResult = ec.service.sync().name("co.hotwax.sob.order.ShopifyOrderMappingServices.map#OrderTransaction").parameters([shopifyOrderId:shopifyOrderId, shopId:shopId, shopifyTransaction: it]).call()
+                if (ec.message.hasError()) {
+                    ec.logger.warn("Failed to map transaction ${it.id} for order ${shopifyOrderId}")
+                    ec.message.clearErrors()
+                }
+                txMappingResult?.orderPaymentPreference
+            }.findAll { it },
+        returnIdentifications: [returnIdentificationTypeId: "SHOPIFY_RTN_ID", idValue: refundId] + (isOrphan ? [returnIdentificationTypeId: "SHPY_ORPN_RTN_ORD_ID", idValue: ShopifyHelper.resolveShopifyGid(order.id)] : [])
     ]
 ]
